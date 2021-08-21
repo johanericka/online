0af4cb6e0abd859d4e41637490dfe5c5bcc3f30c
johanericka <johanericka@gmail.com>
2021-08-21 16:26:38 +0700

    add email notification peminjaman alat

--- dosen/peminjamanalat-wd-setujui.php
+++ dosen/peminjamanalat-wd-setujui.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require_once('../system/dbconn.php');
+require('../system/dbconn.php');
+require('../system/phpmailer/sendmail.php');
 
 $nodata = mysqli_real_escape_string($dbsurat, $_POST['nodata']);
 date_default_timezone_set("Asia/Jakarta");
@@ -16,5 +17,39 @@ $sql = mysqli_query($dbsurat, "UPDATE peminjamanalat
 					keterangan = '$nosurat',
 					statussurat = 1
 					WHERE no = '$nodata' AND validator3='$nip'");
+
+//cari NIP pembuat surat dulu
+$sql1 = mysqli_query($dbsurat, "SELECT * FROM peminjamanalat WHERE no='$nodata'");
+$dsql1 = mysqli_fetch_array($sql1);
+$nim = $dsql1['nim'];
+
+//cari email pembuat surat dari NIP
+$sql3 = mysqli_query($dbsurat, "SELECT * FROM pengguna WHERE nip='$nim'");
+$dsql3 = mysqli_fetch_array($sql3);
+$namamhs = $dsql3['nama'];
+$emailmhs = $dsql3['email'];
+
+//kirim email
+$surat = 'Ijin Peminjaman Alat';
+$subject = "Pengajuan Surat " . $surat;
+$pesan = "Yth. " . $namamhs . "<br/>
+        <br/>
+		Assalamualaikum wr. wb.
+        <br />
+		<br />
+		Dengan hormat,
+		<br />
+        Pengajuan Surat " . $surat . " anda telah disetujui.<br/>
+        Silahkan klik tombol dibawah ini mencetak Surat tersebut<br/>
+        <br/>
+        <a href='https://saintek.uin-malang.ac.id/online/mahasiswa/peminjamanalat-cetak.php?nodata=$nodata' style=' background-color: #0045CE;border: none;color: white;padding: 8px 16px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;'>Cetak Surat Ijin Penelitian</a><br/>
+        <br/>
+        atau silahkan mencetak melalui website SAINTEK Online di <a href='https://saintek.uin-malang.ac.id/online/'>https://saintek.uin-malang.ac.id/online/</a> apabila tombol diatas tidak berfungsi.<br/>
+        <br/>
+        Wassalamualaikum wr. wb.
+		<br/>
+        <br/>
+        <b>SAINTEK Online</b>";
+sendmail($emailmhs, $namamhs, $subject, $pesan);
 
 header("location:index.php");
--- mahasiswa/peminjamanalat-simpan.php
+++ mahasiswa/peminjamanalat-simpan.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-require_once('../system/dbconn.php');
+require('../system/dbconn.php');
+require('../system/phpmailer/sendmail.php');
+
 date_default_timezone_set("Asia/Jakarta");
 $tanggal = date('Y-m-d H:i:s');
 $nim = mysqli_real_escape_string($dbsurat, $_SESSION['nip']);
@@ -46,5 +48,35 @@ $nipwd1 = $dhasil['nip'];
 $stmt = $dbsurat->prepare("INSERT INTO peminjamanalat (tanggal, nim, nama, prodi, judulskripsi, dosen, pimpinaninstansi, instansi, alamat, namaalat, jumlahalat,tglmulai,tglselesai,validator1,validator2,validator3) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 $stmt->bind_param("ssssssssssssssss", $tanggal, $nim, $nama, $prodi, $judulskripsi, $dosen, $pimpinaninstansi, $instansi, $alamat, $namaalat, $jumlahalat, $tglmulai, $tglselesai, $nipdosen, $nipkaprodi, $nipwd1);
 $stmt->execute();
+
+//kirim email ke dosen pembimbing
+//cari email dosen dari NIP
+$sql3 = mysqli_query($dbsurat, "SELECT * FROM pengguna WHERE nip='$nipdosen'");
+$dsql3 = mysqli_fetch_array($sql3);
+$namadosen = $dsql3['nama'];
+$emaildosen = $dsql3['email'];
+
+//kirim email
+$surat = 'Ijin Peminjaman Alat';
+$subject = "Pengajuan Surat " . $surat . "";
+$pesan = "Yth. " . $namadosen . "<br/>
+        <br/>
+		Assalamualaikum wr. wb.
+        <br />
+		<br />
+		Dengan hormat,
+		<br />
+        Terdapat pengajuan surat " . $surat . " atas nama " . $nama . " di sistem SAINTEK Online.<br/>
+        Silahkan klik tombol dibawah ini untuk melakukan verifikasi surat di website SAINTEK Online<br/>
+        <br/>
+        <a href='https://saintek.uin-malang.ac.id/online/' style=' background-color: #0045CE;border: none;color: white;padding: 8px 16px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;'>Website</a><br/>
+        <br/>
+        atau klik URL berikut ini <a href='https://saintek.uin-malang.ac.id/online/'>https://saintek.uin-malang.ac.id/online/</a> apabila tombol diatas tidak berfungsi.<br/>
+        <br/>
+        Wassalamualaikum wr. wb.
+		<br/>
+        <br/>
+        <b>SAINTEK Online</b>";
+sendmail($emaildosen, $namadosen, $subject, $pesan);
 
 header("location:index.php");
